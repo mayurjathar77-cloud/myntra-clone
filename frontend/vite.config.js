4fffa6ade5ed7f17a6253e43dbcb84671dc64640
Mayur Jathar <mayurjathar77@gmail.com>
2026-03-13 12:22:46 +0530

    new changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Myntra/',
+  base: process.env.NODE_ENV === 'production' ? '/Myntra/' : '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
